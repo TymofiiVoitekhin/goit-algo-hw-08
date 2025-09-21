3ba642d2bc7e03e6bf0397e1937f1b0979acab41
Tymofii Voitekhin <tymofiivoitekhin@gmail.com>
2025-09-21 22:21:13 +0300

    Task 3 edited

--- Task3.py
+++ Task3.py
@@ -8,9 +8,15 @@ cables_lenght_2 = [1,9,5,4,7,3,8,2,6]
 # потрібно додавати спочатку найменші кабелі до вже доданих
 
 def order_by_heap (values):
+    minimal_join = 0
+    
     while values:
         heapq.heapify(values)
-        print ("Беремо кабель довжиною:", heapq.heappop(values))
+        min_value = heapq.heappop(values)
+        minimal_join += min_value
+        print ("Беремо кабель довжиною:", min_value)
+    
+    print("Мінімальне зʼєднання всіх кабелів:", minimal_join)
     
 order_by_heap(cables_lenght_1)
 print("-"*100)
